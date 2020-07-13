1a862f6903bdf8096a930db71c11c7de932c3033
Justin Wozniak <wozzytheprogrammer@gmail.com>
2020-07-13 12:05:00 -0700

    first full commit

---
+++ src/components/sections/FeaturesSplit.js
@@ -0,0 +1,163 @@
+import React from 'react';
+import classNames from 'classnames';
+import { SectionSplitProps } from '../../utils/SectionProps';
+import SectionHeader from './partials/SectionHeader';
+import Image from '../elements/Image';
+
+const propTypes = {
+  ...SectionSplitProps.types
+}
+
+const defaultProps = {
+  ...SectionSplitProps.defaults
+}
+
+const FeaturesSplit = ({
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  invertMobile,
+  invertDesktop,
+  alignTop,
+  imageFill,
+  ...props
+}) => {
+
+  const outerClasses = classNames(
+    'features-split section',
+    topOuterDivider && 'has-top-divider',
+    bottomOuterDivider && 'has-bottom-divider',
+    hasBgColor && 'has-bg-color',
+    invertColor && 'invert-color',
+    className
+  );
+
+  const innerClasses = classNames(
+    'features-split-inner section-inner',
+    topDivider && 'has-top-divider',
+    bottomDivider && 'has-bottom-divider'
+  );
+
+  const splitClasses = classNames(
+    'split-wrap',
+    invertMobile && 'invert-mobile',
+    invertDesktop && 'invert-desktop',
+    alignTop && 'align-top'
+  );
+
+  const sectionHeader = {
+    title: ' Whats New',
+    paragraph: 'Heres where Ill post whats new...'
+  };
+
+  return (
+    <section
+      {...props}
+      className={outerClasses}
+    >
+      <div className="container">
+        <div className={innerClasses}>
+          <SectionHeader data={sectionHeader} className="center-content" />
+          <div className={splitClasses}>
+
+            <div className="split-item">
+              <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
+                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                Character Overview
+                  </div>
+                <h3 className="mt-0 mb-12">
+               Raul Duke
+                  </h3>
+                <p className="m-0">
+                Raoul Duke is the fictional character and antihero based on Hunter S. Thompson in his autobiographical novel Fear and Loathing in Las Vegas. The book was originally written under the name Raoul Duke.
+                  </p>
+              </div>
+              <div className={
+                classNames(
+                  'split-item-image center-content-mobile reveal-from-bottom',
+                  imageFill && 'split-item-image-fill'
+                )}
+                data-reveal-container=".split-item">
+                <Image
+                  src={require('./../../assets/images/Characters/raulduke.jpg')}
+                  alt="Features split 01"
+                  width={528}
+                  height={396} />
+              </div>
+            </div>
+
+            <div className="split-item">
+              <div className="split-item-content center-content-mobile reveal-from-right" data-reveal-container=".split-item">
+                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+               Band Spotlight
+                  </div>
+                <h3 className="mt-0 mb-12">
+                The Hollywood Vampires
+                  </h3>
+                <p className="m-0">
+                The Hollywood Vampires are an American rock supergroup formed in 2015 by Alice Cooper, 
+                Johnny Depp, and Joe Perry to honor the music of the rock stars who died from excess in the 1970s. 
+                The band name derives from The Hollywood Vampires, a celebrity drinking club formed by Cooper in 
+                the 1970s which included but was not limited to: John Lennon and Ringo Starr of The Beatles, 
+                Keith Moon of The Who, and Micky Dolenz of The Monkees. Touring members include or have included 
+                Duff McKagan and Matt Sorum of Guns N' Roses fame, as well as Robert DeLeo from Stone Temple Pilots.
+                
+                The band has released two studio albums, featuring guest appearances by Paul McCartney, 
+                Dave Grohl, Joe Walsh, and Christopher Lee among others.
+                  </p>
+              </div>
+              <div className={
+                classNames(
+                  'split-item-image center-content-mobile reveal-from-bottom',
+                  imageFill && 'split-item-image-fill'
+                )}
+                data-reveal-container=".split-item">
+                <Image
+                  src={require('./../../assets/images/Bands/hollywoodvampires.jpeg')}
+                  alt="Features split 02"
+                  width={528}
+                  height={396} />
+              </div>
+            </div>
+
+            <div className="split-item">
+              <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
+                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                Movie Overview
+                  </div>
+                <h3 className="mt-0 mb-12">
+              Blow
+                  </h3>
+                <p className="m-0">
+                The story of how George Jung, along with the Medellín Cartel headed by Pablo Escobar, established the American cocaine market in the 1970s in the United States.
+                  </p>
+              </div>
+              <div className={
+                classNames(
+                  'split-item-image center-content-mobile reveal-from-bottom',
+                  imageFill && 'split-item-image-fill'
+                )}
+                data-reveal-container=".split-item">
+                <Image
+                  src={require('./../../assets/images/Movies/Blow.jpg')}
+                  alt="Features split 03"
+                  width={528}
+                  height={396} />
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+FeaturesSplit.propTypes = propTypes;
+FeaturesSplit.defaultProps = defaultProps;
+
+export default FeaturesSplit;
---
+++ src/views/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import sections
+import Hero from '../components/sections/Hero';
+import FeaturesTiles from '../components/sections/FeaturesTiles';
+import FeaturesSplit from '../components/sections/FeaturesSplit';
+import Cta from '../components/sections/Cta';
+
+const Home = () => {
+
+  return (
+    <>
+      <Hero className="illustration-section-01" />
+      <FeaturesTiles />
+      <FeaturesSplit invertMobile topDivider imageFill className="illustration-section-02" />
+      {/* <Cta split /> */}
+    </>
+  );
+}
+
+export default Home;
