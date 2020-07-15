906707836a80cc6ac4a573b29d59a2b5399aacd3
Justin Wozniak <wozzytheprogrammer@gmail.com>
2020-07-14 19:30:49 -0700

    Finished Bio
    Finished bio section for now

--- src/views/Bio.js
+++ src/views/Bio.js
@@ -1,5 +1,7 @@
 import React from 'react';
-
+import babydepp from '../../src/assets/images/johnnybaby.jpg';
+import babydepp2 from '../../src/assets/images/johnnybaby2.jpg';
+import babydepp3 from '../../src/assets/images/johnnybaby3.jpg';
 
 import { useAsync } from 'react-async';
 
@@ -23,9 +25,23 @@ const Bio = (props) => {
 	}
 
 	return (
-    <div>
-    <div>Biodsdsds</div>
-    </div>
+		<div className="row">
+			<div className="col-1-of-2">
+				<h3 className="heading-tertiary u-margin-bottom-small">Like Something out of a movie...</h3>
+				<p className="paragraph">
+					{data}
+				</p>
+
+			</div>
+			<div className="col-1-of-2">
+				<div className="composition">
+					<img src={babydepp} alt="Pic 1" className="composition__photo composition__photo--p1" />
+					<img src={babydepp2} alt="Pic 2" className="composition__photo composition__photo--p2" />
+					<img src={babydepp3} alt="Pic 3" className="composition__photo composition__photo--p3" />
+					
+			</div>
+		</div>
+		</div>
     )
 };
 
