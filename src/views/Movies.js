71c2326f40e60c03239a062b8c69f9b48356bc12
Justin Wozniak <wozzytheprogrammer@gmail.com>
2020-07-15 21:54:16 -0700

    carousel works!
    Got a carousel to work...with cats. Need to implement movies now ;)

--- src/views/Movies.js
+++ src/views/Movies.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Carousel from '../components/elements/Carousel'
-
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Movies = (props) => {
 	
