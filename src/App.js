8773e69c0ed0a5f9b225df400962e76df88de5b4
Justin Wozniak <wozzytheprogrammer@gmail.com>
2020-08-07 21:27:29 -0700

    Heroku
    Tring to uplaod to heroku

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useRef, useEffect } from 'react';
 import { useLocation, Switch } from 'react-router-dom';
 import AppRoute from './utils/AppRoute';
 import ScrollReveal from './utils/ScrollReveal';
+import ReactGA from 'react-ga';
 
 import Bio from '../src/views/Bio'
 import Movies from '../src/views/Movies'
@@ -13,13 +14,13 @@ import LayoutDefault from './layouts/LayoutDefault';
 import Home from './views/Home';
 
 
-// // Initialize Google Analytics
-// ReactGA.initialize(process.env.REACT_APP_GA_CODE);
+// Initialize Google Analytics
+ReactGA.initialize(process.env.REACT_APP_GA_CODE);
 
-// const trackPage = page => {
-//   ReactGA.set({ page });
-//   ReactGA.pageview(page);
-// };
+const trackPage = page => {
+  ReactGA.set({ page });
+  ReactGA.pageview(page);
+};
 
 const App = () => {
 
@@ -27,10 +28,10 @@ const App = () => {
   let location = useLocation();
 
   useEffect(() => {
-   
+    const page = location.pathname;
     document.body.classList.add('is-loaded')
     childRef.current.init();
-    // trackPage(page);
+    trackPage(page);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [location]);
 
